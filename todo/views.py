513fae5cbb2c8859131753da33a0dd9e17e19a9d
leandrotartarini <ldo.tartarini@gmail.com>
2021-12-24 11:12:35 +0000

    Fixed some warnings

--- todo/views.py
+++ todo/views.py
@@ -1,14 +1,8 @@
 import traceback
-
 from django.db import IntegrityError
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-
 from todo.models import TodoItem
 from todo.serializers import TodoItemSerializer
 
@@ -33,6 +27,7 @@ class TodoView(APIView):
         except IntegrityError:
             return Response({"message": "Error"}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class TodoDetailView(APIView):
 
     def get(self, request, todo_item_id: int):
@@ -44,7 +39,6 @@ class TodoDetailView(APIView):
             return Response({"message": "Item does not exist"}, status=status.HTTP_404_NOT_FOUND)
         except Exception:
             return Response({"message": "Error"}, status=status.HTTP_400_BAD_REQUEST)
-
 
     def delete(self, request, todo_item_id: int) -> Response:
 
@@ -72,9 +66,6 @@ class TodoDetailView(APIView):
         except Exception as e:
             print(traceback.format_exc())
             return Response({"message": "Error"}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 
 #
 # @api_view(http_method_names=['GET', 'POST'])
